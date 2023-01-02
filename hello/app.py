4cfa18143a8cba012ac52db1a12a1bcda88ecce6
Kamil Szatkowski <kamil.szatkowski1994@gmail.com>
2023-01-02 19:55:14 +0100

    URL params exercise 1

--- hello/app.py
+++ hello/app.py
@@ -9,9 +9,15 @@ def index():
     return f'''<h1>Hello world</h1>
     and __name__ is: {__name__}'''
 
+
 @app.route('/about')
 def about():
     return "Flask programming time!!!"
+
+
+@app.route('/cantor/<currency>/<amount>/')
+def cantor(currency, amount):
+    return "{} - {}".format(currency, amount)
 
 
 if __name__ == "__main__":
