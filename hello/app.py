85d9113d3817c8517b25df5a8c6a7736bd017f33
Kamil Szatkowski <kamil.szatkowski1994@gmail.com>
2023-01-02 15:44:06 +0100

    added 'about' and 'if __name__'

--- hello/app.py
+++ hello/app.py
@@ -8,3 +8,11 @@ app = Flask(__name__)
 def index():
     return f'''<h1>Hello world</h1>
     and __name__ is: {__name__}'''
+
+@app.route('/about')
+def about():
+    return "Flask programming time!!!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
